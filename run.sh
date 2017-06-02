e0454c175466b8d79d7676f8d358d94fa10b7eea
v-zhidu <du_zhi_qiang@163.com>
2017-06-02 12:11:18 +0800

    修改requirements.txt

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
-export PYTHONPATH=/Users/duzhiqiang/Code/tribes/tribes
 export DEV_DATABASE_URL=mysql://root:tribes@121.42.244.187:3382/tribes
-export FLASK_APP=/Users/duzhiqiang/Code/tribes/tribes/tribes.py
+export FLASK_APP=${PWD}/tribes/tribes.py
 venv/bin/flask run -h 127.0.0.1 -p 5001
